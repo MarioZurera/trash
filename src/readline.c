a8b337f290bd3b08a063a7bc858056a6edae3636
mzurera- <mzurera-@student.42malaga.com>
2024-10-14 21:16:51 +0200

    feat: readline show prompt before input

--- src/readline.c
+++ src/readline.c
@@ -2,10 +2,11 @@
 
 #include "trash.h"
 
-char *readline(const char *prompt [[maybe_unused]]) {
+char *readline(const char *prompt) {
     constexpr auto MAX_CHARS = 200;
+
+    printf(prompt);
     char *line = malloc(MAX_CHARS + 1);
     fgets(line, MAX_CHARS, stdin);
-    // line[0] = '\0';
     return line;
 }
